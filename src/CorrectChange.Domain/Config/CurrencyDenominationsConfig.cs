79712c22f61a47d4aaf7283afb368f4a8d0c6f28
Chris Russell <cgrussell@gmail.com>
2023-05-12 10:24:59 +0100

    Some basic models for representing the data

---
+++ src/CorrectChange.Domain/Config/CurrencyDenominationsConfig.cs
@@ -0,0 +1,10 @@
+﻿using CorrectChange.Domain.Models;
+
+namespace CorrectChange.Domain.Config
+{
+    public class CurrencyDenominationsConfig
+    {
+        public Currency Currency { get; set; }
+        public List<Denomination> Denominations { get; set; } = new();
+    }
+}
---
+++ src/CorrectChange.Domain/Models/Denomination.cs
@@ -0,0 +1,23 @@
+﻿namespace CorrectChange.Domain.Models
+{
+    /// <summary>
+    ///     Represents the coin or note, whether it is a coin or note, and the currency denominated in
+    /// </summary>
+    public sealed class Denomination
+    {
+        /// <summary>
+        ///     The value of the denomination
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        ///     The type of denomination (Coin, note, etc)
+        /// </summary>
+        public DenominationType DenominationType { get; set; }
+
+        /// <summary>
+        ///     The currency of the denomination
+        /// </summary>
+        public Currency Currency { get; set; }
+    }
+}
