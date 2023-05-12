83380f8910b7ebfd2879fb7715bdc93780e1429a
Chris Russell <cgrussell@gmail.com>
2023-05-12 11:19:54 +0100

    Testing DenominationQuantity value calculations

---
+++ src/CorrectChange.Tests/Denominations/Data/DenominationQuantityValueData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using CorrectChange.Domain.Models;
+
+namespace CorrectChange.Tests.Denominations.Data
+{
+    public class DenominationQuantityValueData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            // Amount
+            // Quantity
+            // Currency,
+            // Expected
+            yield return new object[] { 10, 1, Currency.Euro, 10 };
+            yield return new object[] { 10, 1, Currency.UnitedStatesDollar, 10 };
+            yield return new object[] { 10, 1, Currency.ChinaRenminbi, 10 };
+            yield return new object[] { 10, 1, Currency.Sterling, 10 };
+            yield return new object[] { 10, 1, Currency.AustraliaDollar, 10 };
+            
+            yield return new object[] { 10, 2, Currency.Euro, 20 };
+            yield return new object[] { 10, 2, Currency.UnitedStatesDollar, 20 };
+            yield return new object[] { 10, 2, Currency.ChinaRenminbi, 20 };
+            yield return new object[] { 10, 2, Currency.Sterling, 20 };
+            yield return new object[] { 10, 2, Currency.AustraliaDollar, 20 };
+            
+            yield return new object[] { 20, 1, Currency.Euro, 20 };
+            yield return new object[] { 20, 1, Currency.UnitedStatesDollar, 20 };
+            yield return new object[] { 20, 1, Currency.ChinaRenminbi, 20 };
+            yield return new object[] { 20, 1, Currency.Sterling, 20 };
+            yield return new object[] { 20, 1, Currency.AustraliaDollar, 20 };
+            
+            yield return new object[] { 20, 2, Currency.Euro, 40 };
+            yield return new object[] { 20, 2, Currency.UnitedStatesDollar, 40 };
+            yield return new object[] { 20, 2, Currency.ChinaRenminbi, 40 };
+            yield return new object[] { 20, 2, Currency.Sterling, 40 };
+            yield return new object[] { 20, 2, Currency.AustraliaDollar, 40 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/CorrectChange.Tests/Denominations/DenominationQuantityTests.cs
@@ -0,0 +1,45 @@
+﻿using CorrectChange.Domain.Models;
+using CorrectChange.Tests.Denominations.Data;
+using FluentAssertions;
+
+namespace CorrectChange.Tests.Denominations
+{
+    public class DenominationQuantityTests
+    {
+        [Theory]
+        [ClassData(typeof(DenominationQuantityValueData))]
+        public void Given_DenominationQuantity_Calculated_Value_Is_Correct(decimal amount, int quantity,
+            Currency currency,
+            decimal expectedValue)
+        {
+            var denominationQuantity = MakeDenominationQuantity(amount, quantity, currency);
+
+            denominationQuantity
+                .Value
+                .Should()
+                .Be(expectedValue);
+        }
+
+        /// <summary>
+        ///     Everything's a note. Doesn't figure into the calculation
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="quantity"></param>
+        /// <param name="currency"></param>
+        /// <returns></returns>
+        private static DenominationQuantity MakeDenominationQuantity(decimal amount, int quantity, Currency currency)
+        {
+            return new DenominationQuantity
+            {
+                Denomination = new Denomination
+                {
+                    Amount = amount,
+                    Currency = currency,
+                    DenominationType = DenominationType.Note
+                },
+
+                Quantity = quantity
+            };
+        }
+    }
+}
