2a54370d1ea4604d109ee67d4420b43908bb87ac
Chris Russell <cgrussell@gmail.com>
2023-05-12 11:20:15 +0100

    ChangeService and interface

---
+++ src/CorrectChange.Domain/Services/ChangeService.cs
@@ -0,0 +1,20 @@
+﻿using CorrectChange.Domain.Models;
+
+namespace CorrectChange.Domain.Services
+{
+    public interface IChangeService
+    {
+        IList<DenominationQuantity> CalculateChange(decimal price, decimal amountTendered, Currency currency);
+    }
+
+    /// <summary>
+    ///     Calculates change for specified currency
+    /// </summary>
+    public class ChangeService : IChangeService
+    {
+        public IList<DenominationQuantity> CalculateChange(decimal price, decimal amountTendered, Currency currency)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
