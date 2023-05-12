3d17821e8760869328dbea226abdaf0a4c7cef21
Chris Russell <cgrussell@gmail.com>
2023-05-12 12:10:17 +0100

    Refactored namespace and introduced an IChangeStrategy interface

---
+++ src/CorrectChange.Domain/Services/Change/ChangeStrategies/Abstract/IChangeStrategy.cs
@@ -0,0 +1,9 @@
+﻿using CorrectChange.Domain.Models;
+
+namespace CorrectChange.Domain.Services.Change.ChangeStrategies.Abstract
+{
+    public interface IChangeStrategy
+    {
+        Denomination[] CalculateChange(decimal price, decimal paymentReceived);
+    }
+}
---
+++ src/CorrectChange.Domain/Services/Change/ChangeStrategies/PreferNotesChangeStrategy.cs
@@ -0,0 +1,25 @@
+﻿using CorrectChange.Domain.Config;
+using CorrectChange.Domain.Models;
+using CorrectChange.Domain.Services.Change.ChangeStrategies.Abstract;
+using Microsoft.Extensions.Logging;
+
+namespace CorrectChange.Domain.Services.Change.ChangeStrategies
+{
+    public class PreferNotesChangeStrategy : IChangeStrategy
+    {
+        private readonly CurrencyDenominationsConfig _currencyDenominationsConfig;
+        private readonly ILogger<PreferNotesChangeStrategy> _logger;
+
+        public PreferNotesChangeStrategy(CurrencyDenominationsConfig currencyDenominationsConfig,
+            ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<PreferNotesChangeStrategy>();
+            _currencyDenominationsConfig = currencyDenominationsConfig;
+        }
+
+        public Denomination[] CalculateChange(decimal price, decimal paymentReceived)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
