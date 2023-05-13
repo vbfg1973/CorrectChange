642ff1bb1cb9e23b9069b1bf74715d12979b4b98
Chris Russell <cgrussell@gmail.com>
2023-05-13 06:40:25 +0100

    - CLI displaying output - Data Configured for Euros and UK Sterling - Extension methods for totting up change

---
+++ src/CorrectChange/Verbs/Change/ChangeVerb.cs
@@ -0,0 +1,50 @@
+﻿using CorrectChange.Domain.Extensions;
+using CorrectChange.Domain.Models;
+using CorrectChange.Domain.Services.ChangeCalculator.Abstract;
+using Microsoft.Extensions.Logging;
+
+namespace CorrectChange.Verbs.Change
+{
+    public class ChangeVerb
+    {
+        private readonly IChangeCalculatorService _changeCalculatorService;
+        private readonly ILogger<ChangeVerb> _logger;
+
+        public ChangeVerb(IChangeCalculatorService changeCalculatorService, ILogger<ChangeVerb> logger)
+        {
+            _changeCalculatorService = changeCalculatorService;
+            _logger = logger;
+        }
+
+        public void Run(ChangeOptions options)
+        {
+            var changeDenominations = _changeCalculatorService.CalculateChange(
+                options.Price,
+                options.PaymentReceived,
+                options.Currency,
+                options.ChangeStrategy
+            ).ToList();
+
+            ShowOutput(options, changeDenominations);
+        }
+
+        private static void ShowOutput(ChangeOptions options, List<DenominationQuantity> changeDenominations)
+        {
+            Console.WriteLine($"Currency: {options.Currency}");
+            Console.WriteLine();
+            
+            var sumOfChange = changeDenominations.SumChange();            
+            Console.WriteLine($"Total change: {sumOfChange}");
+            Console.WriteLine();
+
+            foreach (var denominationQuantity in changeDenominations)
+            {
+                Console.WriteLine(
+                    $"\t{denominationQuantity.Quantity} x {denominationQuantity.Denomination.Value} : {denominationQuantity.Denomination.DenominationType}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Verification: {sumOfChange} + {options.Price} = {sumOfChange + options.Price}");
+        }
+    }
+}
