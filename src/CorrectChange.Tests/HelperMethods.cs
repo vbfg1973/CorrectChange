e27b315848192a033f1d634ee45126d402b4b456
Chris Russell <cgrussell@gmail.com>
2023-05-13 08:30:55 +0100

    Documentation comments for test project helper methods

--- src/CorrectChange.Tests/HelperMethods.cs
+++ src/CorrectChange.Tests/HelperMethods.cs
@@ -5,6 +5,11 @@ namespace CorrectChange.Tests
 {
     public static class HelperMethods
     {
+        /// <summary>
+        ///     Uses the Towel library to read XML Documentation Comments. Production of XML Documentation needs to be enabled for
+        ///     each assembly where tests using this methodology are applied
+        /// </summary>
+        /// <param name="assembly"></param>
         public static void LoadXmlDocumentation(this Assembly assembly)
         {
             var directoryPath = assembly.GetDirectoryPath();
